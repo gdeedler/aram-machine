deb72c0316140fe8d8db3cdf07b53b3cfa28aa57
gdeedler <grantdeedler@gmail.com>
2023-07-10 21:54:37 -0400

    Add keystones

---
+++ lib/keystones.ts
@@ -0,0 +1,37 @@
+import ArcaneComet from '../public/keystones/ArcaneComet.png'
+import Conqueror from '../public/keystones/Conqueror.png'
+import DarkHarvest from '../public/keystones/DarkHarvest.png'
+import Electrocute from '../public/keystones/Electrocute.png'
+import FirstStrike from '../public/keystones/FirstStrike.png'
+import FleetFootwork from '../public/keystones/FleetFootwork.png'
+import GlacialAugment from '../public/keystones/GlacialAugment.png'
+import GraspOfTheUndying from '../public/keystones/GraspOfTheUndying.png'
+import Guardian from '../public/keystones/Guardian.png'
+import HailOfBlades from '../public/keystones/HailOfBlades.png'
+import LethalTempo from '../public/keystones/LethalTempo.png'
+import PhaseRush from '../public/keystones/PhaseRush.png'
+import Predator from '../public/keystones/Predator.png'
+import PressTheAttack from '../public/keystones/PressTheAttack.png'
+import SummonAery from '../public/keystones/SummonAery.png'
+import UnsealedSpellbook from '../public/keystones/UnsealedSpellbook.png'
+import Aftershock from '../public/keystones/Aftershock.png'
+
+export default {
+  ArcaneComet,
+  Conqueror,
+  DarkHarvest,
+  Electrocute,
+  FirstStrike,
+  FleetFootwork,
+  GlacialAugment,
+  GraspOfTheUndying,
+  Guardian,
+  HailOfBlades,
+  LethalTempo,
+  PhaseRush,
+  Predator,
+  PressTheAttack,
+  SummonAery,
+  UnsealedSpellbook,
+  Aftershock
+}
